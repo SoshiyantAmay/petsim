cc4d52d8fcae2cd474bef406234521fb3871c636
Soshiyant Amay <soshiyant.amay@gmail.com>
2024-12-27 22:32:37 -0700

    Single pet CLI with improvements.

--- src/globals.lua
+++ src/globals.lua
@@ -1,6 +1,22 @@
-package.path = "./?.lua;" .. package.path
-package.path = "./src/?.lua;" .. package.path
-package.path = "./lib/?.lua;" .. package.path
+-- Add the root, src and lib directories to the package path
+package.path = "./?.lua;" .. "./src/?.lua;" .. "./lib/?.lua;" .. package.path
 
+-- Load Globals
 Utils = require("utils")
 Config = require("config")
+
+-- Game Difficulty Levels (mimicking enums)
+Difficulty = {
+	EASY = 1,
+	NORMAL = 2,
+	HARD = 3,
+	get_name = function(choice)
+		if choice == Difficulty.EASY then
+			return "easy"
+		elseif choice == Difficulty.HARD then
+			return "hard"
+		else
+			return "normal"
+		end
+	end,
+}
